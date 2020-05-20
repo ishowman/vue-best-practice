4a6ffd2795ed8caf6d94b1991ad3ed42eef8bdc7
Bertram <BertramXiao@boyaa.com>
2020-05-20 14:49:36 +0800

    fix(package): update

--- src/main.js
+++ src/main.js
@@ -3,20 +3,21 @@ import App from "./App.vue";
 import router from "./router";
 
 Vue.config.productionTip = false;
-/* eslint-disable */
 registerGlobalAsyncComponents();
 
-async function  registerGlobalAsyncComponents() {
-  
-  const files = require.context("@/components", false, /\.vue$/i, "lazy").keys(); // works
+async function registerGlobalAsyncComponents() {
+  const files = require
+    .context("@/components", false, /\.vue$/i, "lazy")
+    .keys(); // works
   // const files = require.context(folderPath, true, /\.vue$/i, "lazy").keys(); // not work. https://stackoverflow.com/questions/45327765/pass-a-variable-to-require-context?
-  for(const file of files) {
+  for (const file of files) {
     const fileName = file
       .split("/")
       .pop()
       .split(".")[0];
     Vue.component(fileName, () =>
-      import(/* webpackChunkName: "[request]" */`@/components/${fileName}`));
+      import(/* webpackChunkName: "[request]" */ `@/components/${fileName}`)
+    );
   }
 }
 
